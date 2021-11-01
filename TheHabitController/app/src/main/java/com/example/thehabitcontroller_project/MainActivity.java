a26f47da5722d9a6040dfcd48c48f15c296892ab
Mainul Islam <mainul1@ualberta.ca>
2021-11-01 04:33:24 -0600

    Created interface

--- TheHabitController/app/src/main/java/com/example/thehabitcontroller_project/MainActivity.java
+++ TheHabitController/app/src/main/java/com/example/thehabitcontroller_project/MainActivity.java
@@ -1,8 +1,14 @@
 package com.example.thehabitcontroller_project;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigationView);
+        NavController navController = Navigation.findNavController(this,  R.id.fragmentContainerView2);
+        //For changing title
+        //AppBarConfiguration appBarConfiguration = new AppBarConfiguration(R.id.home, R.id.events,R.id.habits,R.id.community).build();
+        NavigationUI.setupWithNavController(bottomNavigationView, navController);
     }
 }
