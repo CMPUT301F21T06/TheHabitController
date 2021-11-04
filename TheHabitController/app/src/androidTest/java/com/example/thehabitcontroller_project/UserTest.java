6dcd360930ddb3f97044e3453544ce082dcc24d0
Weitao(Marcus) Huang <weitao@ualberta.ca>
2021-11-04 17:16:37 -0600

    Fix testcases

--- TheHabitController/app/src/androidTest/java/com/example/thehabitcontroller_project/UserTest.java
+++ TheHabitController/app/src/androidTest/java/com/example/thehabitcontroller_project/UserTest.java
@@ -1,5 +1,6 @@
 package com.example.thehabitcontroller_project;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.fail;
 
 import android.util.Log;
@@ -17,15 +18,16 @@ public class UserTest {
             @Override
             public void onAuthComplete(User u) {
                 Log.d("TestLogin","Username: "+u.getUserName());
+                assertEquals("Test",u.getUserName());
                 return;
             }
         });
-        Thread.sleep(3000);
+        Thread.sleep(2000);
         loginUser.signOut();
         Thread.sleep(1000);
     }
 
-    @Test
+    //@Test
     public void testRegister() throws InterruptedException {
         String username=String.valueOf(Instant.now().getEpochSecond());
         User newUser = User.Register(username+"@test.com",username, "123456");
@@ -36,19 +38,6 @@ public class UserTest {
         String username=String.valueOf(Instant.now().getEpochSecond());
         User newUser = User.Register("test@test.com","Test", "123456");
         Thread.sleep(4000);
-    }
-
-    @Test
-    public void testAuth() throws InterruptedException {
-        User loginUser = new User("test@test.com", "123456", new User.UserAuthListener() {
-            @Override
-            public void onAuthComplete(User u) {
-                Log.d("UserTest",u.getUserName());
-            }
-        });
-        Thread.sleep(2000);
-        loginUser.signOut();
-        Thread.sleep(1000);
     }
 
     @Test
@@ -73,7 +62,7 @@ public class UserTest {
     @Test
     public void testUserSearch() throws InterruptedException{
         ArrayList<Map<String, Object>> gres=new ArrayList<>();
-        User.searchUser("te", new User.UserSearchListener() {
+        User.searchUser("Te", new User.UserSearchListener() {
             @Override
             public void onSearchComplete(ArrayList<Map<String, Object>> result) {
                 gres.addAll(result);
@@ -84,8 +73,7 @@ public class UserTest {
         boolean flag=false;
         for (Map<String, Object> u:gres){
             Log.d("UserSearchTest",u.toString());
-            if (((String) u.get("name")).compareTo("test")==0) {
-                Log.d("UserSearchTest","Found");
+            if (((String) u.get("name")).compareTo("Test")==0) {
                 return;
             }
         }
