2378d9471802c821c3cf9f34ddab96f67a096fc9
Weitao (Marcus) Huang <weitao@ualberta.ca>
2021-11-04 22:37:26 -0600

    Fix first login (new user) behavior

--- TheHabitController/app/src/main/java/com/example/thehabitcontroller_project/MainActivity.java
+++ TheHabitController/app/src/main/java/com/example/thehabitcontroller_project/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.thehabitcontroller_project;
 
 import static androidx.navigation.Navigation.findNavController;
 
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
@@ -12,10 +14,14 @@ import androidx.navigation.ui.NavigationUI;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.FirebaseAuthUIActivityResultContract;
+import com.firebase.ui.auth.data.model.FirebaseAuthUIAuthenticationResult;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 import java.util.Arrays;
 
@@ -34,6 +40,26 @@ import java.util.Arrays;
 public class MainActivity extends AppCompatActivity {
     BottomNavigationView bottomNavigationView;
     NavController navController;
+
+    private final ActivityResultLauncher<Intent> signInLauncher = registerForActivityResult(
+            new FirebaseAuthUIActivityResultContract(),
+            new ActivityResultCallback<FirebaseAuthUIAuthenticationResult>() {
+                @Override
+                public void onActivityResult(FirebaseAuthUIAuthenticationResult result) {
+                    if (result.getResultCode()==RESULT_OK){
+                        Log.d("SignIn","Activity result");
+                        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+                        User.setCurrentUser(new User(user));
+                        if (result.getIdpResponse().isNewUser()){
+                            Log.d("SignIn","Found new user");
+                            User.firstLogin();
+                            Log.d("SignIn",User.getCurrentUser().getUserName());
+                        }
+                    }
+                }
+            }
+    );
+
     @Override
     /**
      * @param savedInstanceState will get the Bundle null when activity get starts first time and it will get in use when activity get changed
@@ -47,7 +73,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
                 if (firebaseAuth.getCurrentUser()!=null){
-                    User.setCurrentUser(new User(firebaseAuth.getCurrentUser()));
+                    Log.d("SignIn","AuthStateChanged");
                 } else {
                     signIn();
                 }
@@ -72,7 +98,7 @@ public class MainActivity extends AppCompatActivity {
                 .setAvailableProviders(Arrays.asList(new AuthUI.IdpConfig.EmailBuilder().build()))
                 .setIsSmartLockEnabled(false)
                 .build();
-        startActivity(loginIntent);
+        signInLauncher.launch(loginIntent);
     }
 
     /**
