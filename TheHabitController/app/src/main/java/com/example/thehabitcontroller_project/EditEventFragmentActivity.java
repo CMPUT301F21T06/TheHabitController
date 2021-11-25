c1b56d779585df9b56c5c4c330ab9ed061d1cdd7
Tyler Mah <55634227+TylerMah@users.noreply.github.com>
2021-11-24 18:16:26 -0700

    can edit and delete events

--- TheHabitController/app/src/main/java/com/example/thehabitcontroller_project/EditEventFragmentActivity.java
+++ TheHabitController/app/src/main/java/com/example/thehabitcontroller_project/EditEventFragmentActivity.java
@@ -1,64 +1,61 @@
 package com.example.thehabitcontroller_project;
 
-import android.app.Activity;
 import android.app.DatePickerDialog;
-import android.content.Context;
-import android.location.Location;
 import android.os.Bundle;
-
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentActivity;
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.CheckBox;
-import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
+
+import com.google.android.material.chip.Chip;
 
 import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.List;
 
 /**
- * A {@link Fragment} subclass to allow adding of {@link Event} objects to the event list
+ * A {@link Fragment} subclass to allow editing of {@link Event}s of the user
+ *
  * @author Tyler
  * @version 1.0.0
  */
-public class AddEventFragmentActivity extends Fragment{
+public class EditEventFragmentActivity extends Fragment {
 
     private EditText title;
     private EditText comment;
     private TextView date;
+    private EditText location;
+    private EditText bitmapString;
     private Button setDateButton;
-    private Button addButton;
+    private Button saveButton;
     private Button cancelButton;
-    private Location location;
-    private Button addLocationButton;
-    private String bitmapString;
-    private Button addPhotoButton;
-    private Habit habit;
+    private Button deleteButton;
 
-    public AddEventFragmentActivity() {
+    public EditEventFragmentActivity() {
         // Required empty public constructor
     }
 
     /**
-     * Override for extending the {@link Fragment} class that inflates the fragment layout
+     * Override for extending the {@link Fragment} class that inflates
+     * the fragment layout
      */
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_add_event_activity, container, false);
+        return inflater.inflate(R.layout.fragment_edit_event_activity, container, false);
     }
 
     /**
@@ -66,35 +63,42 @@ public class AddEventFragmentActivity extends Fragment{
      * calls its parent's implementation of onCreate()
      */
     @Override
-    public void onCreate(Bundle savedInstanceState) { super.onCreate(savedInstanceState); }
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
 
     /**
      * Override for extending the {@link Fragment} class for handling
      * building the structures after the view is created and also setting functionality
-     * for adding events
+     * for editing events
      */
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        // initialize all UI references used
-        addButton = view.findViewById(R.id.addEventButton);
-        cancelButton = view.findViewById(R.id.cancelAddEventButton);
+        // initialize all UI references to be used later
+        saveButton = view.findViewById(R.id.saveEventButton);
+        cancelButton = view.findViewById(R.id.cancelEditEventButton);
+        deleteButton = view.findViewById(R.id.deleteEventButton);
         title = view.findViewById(R.id.event_title);
         comment = view.findViewById(R.id.event_comment);
         date = view.findViewById(R.id.dateEvent_editText);
         setDateButton = view.findViewById(R.id.btPickDateEvent);
-        addLocationButton = view.findViewById(R.id.addEventLocationButton);
-        addPhotoButton = view.findViewById(R.id.addEventPhotoButton);
 
-//        getHabitBundle(view);
+        // get the bundle from the event fragment activity that has the selected event info
+        Bundle currEventBundle = getArguments();
+        Event selectedEvent = currEventBundle.getParcelable("Event");
+        // set the current page with that info
+        title.setText(selectedEvent.getTitle());
+        comment.setText(selectedEvent.getComment());
+        date.setText(selectedEvent.getFormattedDate());
 
         // set the onclicklistener for our set date button to pick a date
         setDateButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 // initialize our DatePicker UI
-                com.example.thehabitcontroller_project.DatePicker date = new com.example.thehabitcontroller_project.DatePicker();
+                DatePicker date = new DatePicker();
 
                 // Set Up Current Date Into dialog
                 Calendar calender = Calendar.getInstance();
@@ -110,12 +114,20 @@ public class AddEventFragmentActivity extends Fragment{
             }
         });
 
-        // set the add button listener
-        addButton.setOnClickListener(new View.OnClickListener() {
+        // set listener for the cancel button; just go back
+        cancelButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                // start a new bundle for transferring the info back to habit list activity
-                Bundle addEventBundle = new Bundle();
+                getActivity().onBackPressed();
+            }
+        });
+
+        // set listener for the save button
+        saveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // start our edited event bundle
+                Bundle editEventBundle = new Bundle();
 
                 // set date to be current date if no input was found
                 Date inputDate = new Date();
@@ -127,53 +139,55 @@ public class AddEventFragmentActivity extends Fragment{
                         e.printStackTrace();
                     }
                 }
-
-                // collects info entered by user
-                String event = title.getText().toString();
+                // get the edited event's info
+                String eventTitle = title.getText().toString();
+                // if no event title is entered, return
+                if (eventTitle.length() == 0) {
+                    getActivity().onBackPressed();
+                    return;
+                }
                 String eventComment = comment.getText().toString();
-                String eventLocation = "location";
-//                Location eventLocation = new Location("Home"); // need to implement
-                String bitmap = "bitmapString"; // need to implement
 
-                // add the new event to the bundle
-                addEventBundle.putParcelable("addEvent", new Event(event, eventComment, inputDate, eventLocation, bitmap));
+                String location = "";
+                String bitmapString = "";
 
-                // navigate to the event list view
+                // put the new event in the bundle and the index of the event from the original list
+                editEventBundle.putParcelable(
+                        "editedEvent",
+                        new Event(eventTitle, eventComment, inputDate, location, bitmapString)
+                );
+                editEventBundle.putInt("index", currEventBundle.getInt("index"));
+
+                // navigate to the event fragment activity
                 Navigation.findNavController(view).navigate(
-                        R.id.action_addEventActivity_to_events,
-                        addEventBundle
+                    R.id.action_editEventFragmentActivity_to_events,
+                    editEventBundle
                 );
             }
         });
 
-        // set listener for canceling to return to previous page
-        cancelButton.setOnClickListener(new View.OnClickListener() {
+        // set listener for the delete button
+        deleteButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                getActivity().onBackPressed();
+                // start new bundle for deleting the event
+                Bundle deleteEventBundle = new Bundle();
+
+                // put the index of the event we want to delete
+                deleteEventBundle.putInt("deleteIndex", currEventBundle.getInt("index"));
+
+                // navigate back to event fragment activity
+                Navigation.findNavController(view).navigate(
+                        R.id.action_editEventFragmentActivity_to_events,
+                        deleteEventBundle
+                );
             }
         });
     }
 
-    private void getHabitBundle(View view) {
-        Bundle currBundle = getArguments();
-        if (!currBundle.isEmpty()) {
-            // see if we've added a Habit
-            Habit currHabit = currBundle.getParcelable("Habit");
-            if (currHabit != null) {
-                this.habit = currHabit;
-            }
-
-            // TODO: Add functionality to this so habit is attached to event
-
-            // clear args so we refresh for next commands
-            getArguments().clear();
-        }
-    }
-
     // attribute for the date listener for setting the date fragment on the page
     DatePickerDialog.OnDateSetListener onDate = new DatePickerDialog.OnDateSetListener() {
-        public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+        public void onDateSet(android.widget.DatePicker view, int year, int monthOfYear, int dayOfMonth) {
             Calendar mCalendar = Calendar.getInstance();
             mCalendar.set(Calendar.YEAR, year);
             mCalendar.set(Calendar.MONTH, monthOfYear);
