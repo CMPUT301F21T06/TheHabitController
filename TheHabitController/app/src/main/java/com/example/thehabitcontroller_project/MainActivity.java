de9a4596055d25275da218a7005680da739f8dd7
Mainul47 <52233037+Mainul47@users.noreply.github.com>
2021-11-29 02:53:19 -0700

    Still needs some work
    Try to connect the follow with button from firebase, so when somebody follow, it will increase the counter and the number will keep increasing.

--- TheHabitController/app/src/main/java/com/example/thehabitcontroller_project/MainActivity.java
+++ TheHabitController/app/src/main/java/com/example/thehabitcontroller_project/MainActivity.java
@@ -2,28 +2,26 @@ package com.example.thehabitcontroller_project;
 
 import static androidx.navigation.Navigation.findNavController;
 
-import androidx.activity.result.ActivityResultCallback;
-import androidx.activity.result.ActivityResultLauncher;
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.TaskStackBuilder;
 import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
 import androidx.navigation.fragment.NavHostFragment;
-import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Button;
 
-import com.firebase.ui.auth.AuthUI;
-import com.firebase.ui.auth.FirebaseAuthUIActivityResultContract;
-import com.firebase.ui.auth.data.model.FirebaseAuthUIAuthenticationResult;
+import com.example.thehabitcontroller_project.Community.CommunityFragmentActivity;
+import com.example.thehabitcontroller_project.Community.User;
+import com.example.thehabitcontroller_project.Event.EventsFragmentActivity;
+import com.example.thehabitcontroller_project.Habit.AddHabitFragmentActivity;
+import com.example.thehabitcontroller_project.Habit.Habit;
+import com.example.thehabitcontroller_project.Habit.HabitsFragmentActivity;
+import com.example.thehabitcontroller_project.Home.HomeFragmentActivity;
+import com.example.thehabitcontroller_project.Login.LoginActivity;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-
-import java.util.Arrays;
 
 /**
  * @author mainul1
@@ -41,44 +39,17 @@ public class MainActivity extends AppCompatActivity {
     BottomNavigationView bottomNavigationView;
     NavController navController;
 
-    private final ActivityResultLauncher<Intent> signInLauncher = registerForActivityResult(
-            new FirebaseAuthUIActivityResultContract(),
-            new ActivityResultCallback<FirebaseAuthUIAuthenticationResult>() {
-                @Override
-                public void onActivityResult(FirebaseAuthUIAuthenticationResult result) {
-                    if (result.getResultCode()==RESULT_OK){
-                        Log.d("SignIn","Activity result");
-                        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
-                        User.setCurrentUser(new User(user));
-                        if (result.getIdpResponse().isNewUser()){
-                            Log.d("SignIn","Found new user");
-                            User.firstLogin();
-                            Log.d("SignIn",User.getCurrentUser().getUserName());
-                        }
-                    }
-                }
-            }
-    );
-
     @Override
     /**
      * @param savedInstanceState will get the Bundle null when activity get starts first time and it will get in use when activity get changed
      */
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        if (User.getCurrentUser()==null) {
+            signIn();
+        }
         setContentView(R.layout.activity_main);
         setUpNavigation();
-        // register firebase authentication listener for login/logout operations
-        FirebaseAuth.getInstance().addAuthStateListener(new FirebaseAuth.AuthStateListener() {
-            @Override
-            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
-                if (firebaseAuth.getCurrentUser()!=null){
-                    Log.d("SignIn","AuthStateChanged");
-                } else {
-                    signIn();
-                }
-            }
-        });
     }
 
     @Override
@@ -93,12 +64,9 @@ public class MainActivity extends AppCompatActivity {
      * Build and launch login intent
      */
     public void signIn() {
-        Intent loginIntent = AuthUI.getInstance()
-                .createSignInIntentBuilder()
-                .setAvailableProviders(Arrays.asList(new AuthUI.IdpConfig.EmailBuilder().build()))
-                .setIsSmartLockEnabled(false)
-                .build();
-        signInLauncher.launch(loginIntent);
+        Log.d("LoginDetect","Start Login Activity");
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
     }
 
     /**
@@ -112,7 +80,57 @@ public class MainActivity extends AppCompatActivity {
         NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.fragmentContainerView2);
         navController = navHostFragment.getNavController();
         //For changing title
-        //AppBarConfiguration appBarConfiguration = new AppBarConfiguration(R.id.home, R.id.events,R.id.habits,R.id.community).build();
         NavigationUI.setupWithNavController(bottomNavigationView, navController);
     }
+
+    /**
+     * This method is called whenever the user chooses to navigate Up within your application's
+     * activity hierarchy from the action bar.
+     *
+     * <p>If a parent was specified in the manifest for this activity or an activity-alias to it,
+     * default Up navigation will be handled automatically. See
+     * {@link #getSupportParentActivityIntent()} for how to specify the parent. If any activity
+     * along the parent chain requires extra Intent arguments, the Activity subclass
+     * should override the method {@link #onPrepareSupportNavigateUpTaskStack(TaskStackBuilder)}
+     * to supply those arguments.</p>
+     *
+     * <p>See <a href="{@docRoot}guide/topics/fundamentals/tasks-and-back-stack.html">Tasks and
+     * Back Stack</a> from the developer guide and
+     * <a href="{@docRoot}design/patterns/navigation.html">Navigation</a> from the design guide
+     * for more information about navigating within your app.</p>
+     *
+     * <p>See the {@link TaskStackBuilder} class and the Activity methods
+     * {@link #getSupportParentActivityIntent()}, {@link #supportShouldUpRecreateTask(Intent)}, and
+     * {@link #supportNavigateUpTo(Intent)} for help implementing custom Up navigation.</p>
+     *
+     * @return true if Up navigation completed successfully and this Activity was finished,
+     * false otherwise.
+     */
+    @Override
+    public boolean onSupportNavigateUp() {
+        return navController.navigateUp();
+    }
 }
+
+
+
+
+
+/***
+ for marcus
+
+
+
+ Button button; // By clicking follow,this button will activate
+
+ notificationCounter = new NotificationCounter(findViewById(R.id.bell));
+ button.setOnClickListner(new View.onClickListner()){
+@Override
+public void onClick(View view){
+notificationCounter.increaseNumber(); // Will increase the number beside the notification until 99, if you feel like it, increase the number
+}
+});
+
+
+
+ */
